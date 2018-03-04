31997a94a4b51450c462735631cd177315ea9df1
Anish Pradhananga <anish.logicseed@gmail.com>
2018-03-04 17:03:24 +0545

    added package.json using npm added initial library functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+//headers here
 
+'use strict';
+
+function position(){
+    console.log("getting position");
+}
+
+function draw(){
+    console.log("drawing");
+}
+
+function erase(){
+    console.log("erasing");
+}
+
+function annotate(){
+    console.log("adding annotation");
+}
+
+module.exports = {
+  position: position,
+  draw: draw,
+  erase: erase,
+  annotate: annotate
+};
